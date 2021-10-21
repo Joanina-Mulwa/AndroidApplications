a03d05df678f4612b5a60b3f6e53ae4ecf40f0ef
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-10-21 14:45:20 +0300

    Updated Whatsapp section

--- ListViewDetails/app/src/main/java/com/example/listviewdetails/Whatsapp.java
+++ ListViewDetails/app/src/main/java/com/example/listviewdetails/Whatsapp.java
@@ -3,12 +3,28 @@ package com.example.listviewdetails;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Whatsapp extends AppCompatActivity {
-
+    Button chats,groups,status,calls;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whatsapp);
+
+        chats = findViewById(R.id.chats);
+        groups = findViewById(R.id.groups);
+        status = findViewById(R.id.status);
+        calls = findViewById(R.id.calls);
+
+        chats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Chats", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
